e118a80a13ed2cff22a2a4f34015d81cac1beef5
Arun Prajapati <arunkp1122@gmail.com>
2021-06-12 18:25:28 +0545

    a

--- useful_notes.py
+++ useful_notes.py
@@ -1,5 +1,7 @@
 dir()
 # Prints the attributes of an object
+obj.__dict__
+# Prints the attributes of an object along with its class and parameters
 
 help()
 # helper function
@@ -22,3 +24,10 @@ print(*object, sep=" ", end="/n", file=sys.stdout, flush=False)
 # if we dont want certain data then represent with _
 #eg: we only want city and popln from tuple. 
 city, _, popln = "Bkt", "Nepal", 100_000_000
+from collections import name
+
+
+import sys
+sys.modules
+
+sys.__file__ # see the file path
